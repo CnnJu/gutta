757951d74ab5b7e0bcac6383f8df982a4424e014
CnnJu <150914244+CnnJu@users.noreply.github.com>
2024-07-08 15:00:28 +0100

    calendar update

--- calendar.js
+++ calendar.js
@@ -15,8 +15,8 @@ document.addEventListener('DOMContentLoaded', function() {
         if (isUpcoming) {
             if (!ticketLink) {
                 eventText = " — <a href='" + eventLink + "'>" + location + "</a> — Tickets Information Coming";
-            } else if (ticketLink === "Free Entry!") {
-                eventText = " — <a href='" + eventLink + "'>" + location + "</a> — Free Entry!";
+            } else if (ticketLink === "Free Entry/Donations") {
+                eventText = " — <a href='" + eventLink + "'>" + location + "</a> — Free Entry/Donations";
             } else {
                 eventText = " — <a href='" + eventLink + "'>" + location + "</a> — <a href='" + ticketLink + "'>tickets</a>";
             }
@@ -40,11 +40,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // Example of adding a new upcoming event
   // Add more events as needed with 'addEvent("date", "Name and place", "link to description", "link to tickets", true);'
-  addEvent("10.05.2024", "Naughty Cabaret, Teaterkjellern, Oslo", "", "https://www.ticketmaster.no/event/731063", true);
-  addEvent("02.06.2024", "Pride House, Bergen Pride", "https://bergenpride.no", "Free Entry!", true);
-  addEvent("07.06.2024", "Porsgrunn International Theatre Festival, Grenland Friteater", "https://pitfestival.no", "", true);
-  addEvent("25.06.2024", "Oslo Pride at SALT", "https://www.oslopride.no", "Free Entry!", true);
+  addEvent("02.08.2024", "Sonntags Club, Berlin ", "https://www.sonntags-club.de/programm/gutta.php", "Free Entry!", true);
+  addEvent("03.08.2024", "Get Fucked, Freiluftkino Hasenheide, Berlin", "https://www.freiluftkino-hasenheide.de/filme/get-fucked-in-hasenheide-olympias-event-27134/", "https://www.kinoheld.de/kino/berlin/freiluftkino-hasenheide/vorstellung/780485", true);
+  addEvent("04.08.2024", "Linienstrasse 206, Berlin", "https://linie206.blackblogs.org/", "Free Entry!", true);
   // Example of adding a new past event
+  addEvent("10.05.2024", "Naughty Cabaret, Teaterkjellern, Oslo", "", "https://www.ticketmaster.no/event/731063", false);
+  addEvent("02.06.2024", "Pride House, Bergen Pride", "https://bergenpride.no", "Free Entry!", false);
+  addEvent("07.06.2024", "Porsgrunn International Theatre Festival, Grenland Friteater", "https://pitfestival.no", "", false);
+  addEvent("25.06.2024", "Oslo Pride at SALT", "https://www.oslopride.no", "Free Entry!", false);
   addEvent("2019-2024", "Gutteklubben at SALT, Skeiv Scene, LOKAL in Trondheim, Elsker Oslo, RAS/ Rogaland på Skeivå", "", "", false);
   addEvent("2019-2024", "Gutta at Pride; Træna Pride, Oslo Pride, Bodø Pride, Drammen Pride, Trondheim Pride", "", "", false);
   addEvent("2023", "Gutta Opening Pride, Oslo Pride", "", "", false);
